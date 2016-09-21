07a0e50f302d434b93b8b46f7fa2612c067437a6
Chris Aporta <christopher.aporta@flatironschool.com>
2016-09-21 17:51:46 -0400

    add Square component and map them to the board with coordinate props

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Square = ({ coordinate }) => (
+    <div
+        className={'square'}
+        id={coordinate}
+    >
+    </div>
+);
+
+export default Square;
