1008daa28c749d3dbd2c399f948e8106a25f62e5
Chris Aporta <christopher.aporta@gmail.com>
2016-10-20 20:52:56 -0400

    subscribe SideSelect to store to provide <select> with a disabled attribute

--- src/components/SideSelect.js
+++ src/components/SideSelect.js
@@ -1,26 +1,46 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import { toggleSide } from '../actions/index';
 
 
-const SideSelect = ({ sides }, { store }) => (
-    <div>
-        <select
-            onChange={(e) =>
-                store.dispatch(toggleSide(e.target.value))
-            }
-        >
-            {sides().map(side =>
-                <option
-                    key={sides().indexOf(side)}
-                    value={side}
+class SideSelect extends Component {
+    componentDidMount() {
+        const { store } = this.context;
+        this.unsubscribe = store.subscribe(() =>
+            this.forceUpdate()
+        );
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+        const props = this.props;
+        const { store } = this.context;
+        const state = store.getState();
+        let disabled = state.game.active ? 'disabled' : ''
+        return (
+            <div>
+                <select
+                    disabled={disabled}
+                    onChange={(e) =>
+                        store.dispatch(toggleSide(e.target.value))
+                    }
                 >
-                    {side}
-                </option>
-            )}
-        </select>
-    </div>
-);
+                    {props.sides().map(side =>
+                        <option
+                            key={props.sides().indexOf(side)}
+                            value={side}
+                        >
+                            {side}
+                        </option>
+                    )}
+                </select>
+            </div>
+        );
+    }
+}
 SideSelect.contextTypes = {
     store: PropTypes.object
 };
