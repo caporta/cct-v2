98dbecaccc6bc253fdbbd948198f9a4fd6864489
Chris Aporta <christopher.aporta@gmail.com>
2016-10-24 19:52:15 -0400

    refactor Board && BoardContainer to make the former fully presentational/dumb.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,32 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import SquareContainer from '../containers/SquareContainer';
 
 
-class Board extends Component {
-    setCoordinates() {
-        const { side, coordinates } = this.props;
-        return side == 'black'
-            ? coordinates().reverse()
-            : coordinates();
-    }
-
-    render() {
-        let coordinates = this.setCoordinates();
-        return (
-            <div
-                className={'board'}
-            >
-                {coordinates.map(coordinate =>
-                    <SquareContainer
-                        key={coordinates.indexOf(coordinate)}
-                        id={coordinate}
-                        coordinates={coordinates}
-                    />
-                )}
-            </div>
-        );
-    }
-}
+const Board = ({ coordinates }) => (
+        <div
+            className={'board'}
+        >
+            {coordinates.map(coordinate =>
+                <SquareContainer
+                    key={coordinates.indexOf(coordinate)}
+                    id={coordinate}
+                    coordinates={coordinates}
+                />
+            )}
+        </div>
+);
 
 export default Board;
--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Board from '../components/Board';
 
 
+class BoardContainer extends Component {
+    setCoordinates() {
+        const { side, coordinates } = this.props;
+        return side == 'black'
+            ? coordinates().reverse()
+            : coordinates();
+    }
+
+    render() {
+        let coordinates = this.setCoordinates();
+        return(
+            <Board
+                coordinates={coordinates}
+            />
+        );
+    }
+}
+
 const mapStateToProps = (state) => ({
     side: state.side
 });
 
-const BoardContainer = connect(
+export default connect(
     mapStateToProps
-)(Board);
-
-export default BoardContainer;
+)(BoardContainer);
