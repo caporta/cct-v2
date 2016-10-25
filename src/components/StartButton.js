ea2f8c7b5488c2c6f17ead2fb749c7e657624478
Chris Aporta <christopher.aporta@gmail.com>
2016-10-25 00:09:10 -0400

    refactor StartButton && StartContainer to make the former fully presentational/dumb.

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,44 +1,18 @@
-import React, { Component, PropTypes } from 'react';
-
-import { toggleGame, resetScore, updateCoordinate, resetCoordinate } from '../actions/index';
-import { getRandomCoordinate } from '../utilities/index';
+import React from 'react';
 
 
-class StartButton extends Component {
-    activateGame() {
-        const { dispatch, coordinates } = this.props;
-        let coordinate = getRandomCoordinate(coordinates());
-        dispatch(resetScore())
-        dispatch(toggleGame());
-        dispatch(updateCoordinate(coordinate));
-    }
-
-    deactivateGame(timeout) {
-        const { dispatch } = this.props;
-        setTimeout(() => {
-            dispatch(toggleGame())
-            dispatch(resetCoordinate())
-        }, timeout)
-    }
-
-    render() {
-        const { active } = this.props;
-        let disabled = active ? 'disabled' : ''
-        return (
-            <div>
-                <button
-                    disabled={disabled}
-                    className={'button button__start'}
-                    onClick={(e) => {
-                        this.activateGame()
-                        this.deactivateGame(30000)
-                    }}
-                >
-                    Start
-                </button>
-            </div>
-        );
-    }
-}
+const StartButton = ({ disabled, onClick }) => (
+    <div>
+        <button
+            disabled={disabled}
+            className={'button button__start'}
+            onClick={(e) => {
+                onClick();
+            }}
+        >
+            Start
+        </button>
+    </div>
+)
 
 export default StartButton;
--- src/containers/StartContainer.js
+++ src/containers/StartContainer.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import StartButton from '../components/StartButton';
+import * as actionCreators from '../actions/index';
+import { getRandomCoordinate } from '../utilities/index';
 
+
+class StartContainer extends Component {
+    activateGame() {
+        const { coordinates, actions } = this.props;
+        let coordinate = getRandomCoordinate(coordinates());
+        actions.resetScore();
+        actions.toggleGame();
+        actions.updateCoordinate(coordinate)
+    }
+
+    deactivateGame(timeout) {
+        const { actions } = this.props;
+        setTimeout(() => {
+            actions.toggleGame();
+            actions.resetCoordinate();
+        }, timeout)
+    }
+
+    handleClick() {
+        this.activateGame()
+        this.deactivateGame(30000)
+    }
+
+    render() {
+        const { active } = this.props;
+        let disabled = active ? 'disabled' : ''
+        return(
+            <StartButton
+                disabled={disabled}
+                onClick={this.handleClick.bind(this)}
+            />
+        );
+    }
+}
 
 const mapStateToProps = (state) => ({
     active: state.game.active
 });
 
-const StartContainer = connect(
-    mapStateToProps
-)(StartButton);
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(actionCreators, dispatch)
+});
 
-export default StartContainer;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(StartContainer);
